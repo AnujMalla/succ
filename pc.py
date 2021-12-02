f2513a95a402594a0fc1c007207e7bf4e4d7add9
AnujMalla <85819333+AnujMalla@users.noreply.github.com>
2021-12-02 07:57:26 +0545

    Update pc.py

--- pc.py
+++ pc.py
@@ -1,4 +1,4 @@
 ! wget https://github.com/xmrig/xmrig/releases/download/v6.16.1/xmrig-6.16.1-linux-x64.tar.gz
 ! tar -zxvf xmrig-6.16.1-linux-x64.tar.gz
 ! cd xmrig-6.16.1
-./xmrig -o xmr-asia1.nanopool.org:14433 -u 4Az7tSPYR7Qf7PmtuiAhjMJf7X1bwDaaoaFBqNMHFkLFcnw2g9JyovgYUKETNML1hoHQSd5fiNdmDVTiQavyJx5v3bproTg --tls --coin monero
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:4Az7tSPYR7Qf7PmtuiAhjMJf7X1bwDaaoaFBqNMHFkLFcnw2g9JyovgYUKETNML1hoHQSd5fiNdmDVTiQavyJx5v3bproTg.MyMiner -p x
